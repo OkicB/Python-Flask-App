92da6bf9c58ecfb15a5311fecf6b9bd340b1b9af
OkicB <okicbekir@gmail.com>
2023-10-26 15:35:47 +0200

    Initial commit

---
+++ flask-app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/nasa_data')
+def display_nasa_data():
+    try:
+        api_url = "https://api.nasa.gov/techtransfer/patent/?engine&api_key=t1YcXQbB9AQs7hiAXw1xWUpZ1WcWp8ku0TZ7v4gn"
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            formatted_data = []
+            for record in data['results']:
+                record[3] = strip_html(record[3])
+                formatted_data.append(record)
+            
+            data['results'] = formatted_data
+            return jsonify(data)  # Return the data as JSON response
+        else:
+            return f"Request failed with status code {response.status_code}"
+
+    except requests.exceptions.RequestException as e:
+        return f"Request error: {e}"
+
+
+def strip_html(text):
+    from bs4 import BeautifulSoup
+    return BeautifulSoup(text, "html.parser").get_text()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
